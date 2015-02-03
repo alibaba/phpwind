222e46744d0465cc54ed3762949af3172a089556
七文34214399@qq.com <dongyong.ydy@alibaba-inc.com>
2015-02-03 14:55:11 +0800

    我的空间，我ta的空间数据合并

--- upload/src/applications/native/controller/SpaceController.php
+++ upload/src/applications/native/controller/SpaceController.php
@@ -12,6 +12,7 @@
 defined('WEKIT_VERSION') || exit('Forbidden');
 
 Wind::import('APPS:native.controller.NativeBaseController');
+Wind::import('SRV:space.bo.PwSpaceModel');
 
 class SpaceController extends NativeBaseController {
 
@@ -32,17 +33,52 @@ class SpaceController extends NativeBaseController {
     public function run(){
         $spaceUid = $this->getInput('uid','get');
         $page = $this->getInput('page','get');
-       
+        
         //
-        $tids           = $this->_getPwNativeThreadDs()->getThreadListByUid($spaceUid, $page, 'space');
+        $space = new PwSpaceModel($spaceUid);
+        $space->setTome($spaceUid, $this->uid);
+        //
+        $tids           = $this->_getPwNativeThreadDs()->getThreadListByUid($spaceUid, $page, $this->uid==$spaceUid?'my':'space');
         $myThreadList   = $this->_getPwNativeThreadDs()->getThreadContent($tids);
         //pids 默认是0； 
         $attList        = $this->_getPwNativeThreadDs()->getThreadAttach($tids, array(0) );
         $threadList     = $this->_getPwNativeThreadDs()->gather($myThreadList, $attList);
+
+        $_allkeys = array_keys($threadList);
+        $barName = $threadList[$_allkeys[0]]['lastpost_time'];
+
+        //
+        $_threadList = array();
+        $prev_val = '';
+        foreach($threadList as $k=>$v){
+            $_created_time = Pw::time2str($v['created_time'],'auto');
+            list($_key, $_time) = explode(' ',$_created_time);
+            if( !preg_match('/-/',$_created_time) ){
+                $_key = '今天';
+            }
+            if( $prev_val!=$_key  ){
+                $threadList[$k]['barName'] = $_key;
+                $prev_val = $_key;
+            }else{
+                $threadList[$k]['barName'] = '';
+            }
+            $_threadList[] = $threadList[$k];
+        }
+
+        //print_r($threadList);exit;
         //
         $data = array(
-            'pageCount'=>$this->_getPwNativeThreadDs()->getThreadPageCount(),
-            'threadList'=>$threadList,
+            'userInfo'  =>isset($space->spaceUser)
+            ?array(
+                'username'  =>$space->spaceUser['username'],
+                'gender'    =>$space->spaceUser['gender'],
+                'location_text'=>$space->spaceUser['location_text'],
+                'avatar'    =>Pw::getAvatar($spaceUid)
+            )
+            :array('username'=>'','gender'=>0,'location_text'=>'','avatar'=>''),
+            'tome'      =>isset($space->tome)?$space->tome:0,
+            'pageCount' =>$this->_getPwNativeThreadDs()->getThreadPageCount(),
+            'threadList'=>$_threadList,
         );
         $this->setOutput($data, 'data');
         $this->showMessage('success');
