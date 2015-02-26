413b73f6f07a96843ef80e8cbc6edd15422f641b
七文34214399@qq.com <dongyong.ydy@alibaba-inc.com>
2015-02-26 19:23:15 +0800

    隐藏标签,回复显示标签解析

--- upload/src/service/native/srv/PwNativeThreadDisplay.php
+++ upload/src/service/native/srv/PwNativeThreadDisplay.php
@@ -14,60 +14,44 @@ Wind::import('SRV:forum.srv.PwThreadDisplay');
 class PwNativeThreadDisplay extends PwThreadDisplay {
 
     public function __construct($tid, PwUserBo $user) {
-		parent::__construct($tid, $user);
+        parent::__construct($tid, $user);
     }
 
     protected function _bulidContent($read) {
-        return self::escapeSpace($read['content']);
-/*
-		if (!$read['useubb']) {
-			return self::escapeSpace($read['content']);
-		}
-        $ubb = new PwUbbCodeConvertThread($this->thread, $read, $this->user);
-        $ubb->isConvertPost = false;
-        $ubb->isconverthide = false;
-        $ubb->isconvertsell = false;
-        $ubb->isconverttao  = false;
+        $_content = self::escapeSpace($read['content']);
+        
+        //检查是否购买过
+        $threadBuy = Wekit::load('forum.PwThreadBuy')->getByTidAndUid($this->tid, $this->user->uid);
+        if( $threadBuy && isset($threadBuy[$read['pid']]) || $this->user->uid==$read['created_userid'] ){
+            //todo
+        }else{
+            $_content = preg_replace("/\[sell=(\d+),(\d+)\]([^\[]+)\[\/sell\]/", '[sell=\\1,\\2]此段为出售的内容，购买后显示[/sell]', $_content);
+        }
+
+        //检查回复后显示
+        if ($read['created_userid'] == $this->user->uid) {
+            //todo
+        } elseif (Wekit::load('forum.PwThread')->countPostByTidAndUid($this->tid, $this->user->uid) > 0) {
+            //todo
+        } else {
+            $_content = preg_replace("/\[post\]([^\[]+)\[\/post\]/", '[post]隐藏内容,回复后显示[/post]', $_content);
+        } 
+
+        //达到积分上限查看贴子
+        $_content = preg_replace("/\[hide=(.+?)\](.+?)\[\/hide\]/eis","self::createHide('\\1', '\\2', '".$this->user->info['credit1']."')", $_content);
 
 
-		$ubb->setImgLazy($this->imgLazy);
-		$this->attach && $this->attach->has($read['pid']) && $ubb->setAttachParser($this->attach);
-		$read['reminds'] && $ubb->setRemindUser($read['reminds']);
-        return PwUbbCode::convert($read['content'], $ubb);
- */
+        return $_content;
     }
 
-    /**
-     * PwUbbCodeConvertConfig 
-     * 
-     * @access protected
-     * @return void
-     */
-    protected function _bulidSellContent($message){
-        Wind::import('LIB:ubb.config.PwUbbCodeConvertConfig');
-        is_null($config) && $config = new PwUbbCodeConvertConfig();
+    protected static function createHide( $costs, $message, $user_credit1 ){
+        list($cost, $credit) = explode(',', $costs);
 
-
-    //    $config->isConvertSell && self::hasTag($message, 'sell') && $message = self::parseSell($message, $config); 
-
+        if( (int)$cost>=intval($user_credit1) ){
+            return "[hide={$costs}]内容加密,需要{$cost}积分以上才能浏览[/hide]";
+        }else{
+            return "[hide={$costs}]{$message}[/hide]";
+        }
     }
-
-    /** 
-     * 检测内容中是否包含标签
-     *
-     * @param string $message 内容
-     * @param string $tag 标签
-     * @return bool
-     */
-    private static function hasTag($message, $tag) {                                                                                                       
-        $startTag = '[' . $tag;
-        $endTag = '[/' . $tag . ']';
-        if (strpos($message, $startTag) !== false && strpos($message, $endTag) !== false) {
-            return true;
-
-        }   
-        return false;
-    }  
-
 
 }
